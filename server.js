e3620b73eb44bc6577f9e1ed4ad12d630ccca944
NANDIMANDALAM-SUNNY <r170911@rguktrkv.ac.in>
2023-02-18 21:58:40 +0530

    hello

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const userRoutes = require('./routes/userRoutes')
 const User = require('./models/User');
 const Message = require('./models/Message')
-const rooms = ['general', 'tech', 'finance', 'crypto'];
+const rooms = [ 'tech', 'finance', 'science' ];
 const cors = require('cors');
 require('dotenv').config()
 
