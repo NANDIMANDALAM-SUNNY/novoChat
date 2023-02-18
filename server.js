2dd90a78438343304075d1155f4f119bf569241d
NANDIMANDALAM-SUNNY <r170911@rguktrkv.ac.in>
2023-02-18 21:52:04 +0530

    commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('morgan');
 const app = express();
 const userRoutes = require('./routes/userRoutes')
 const User = require('./models/User');
@@ -9,13 +10,14 @@ const cors = require('cors');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
+app.use(logger('dev'));
 
 
 app.use('/users', userRoutes)
 require('./connection')
 
 const server = require('http').createServer(app);
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3000',
